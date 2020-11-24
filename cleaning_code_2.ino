ec1aac3f0d79a587ef05ff28ec846fc58e63662e
Ac3r365 <74841936+Ac3r365@users.noreply.github.com>
2020-11-24 01:24:29 -0500

    Add files via upload

---
+++ cleaning_code_2.ino
@@ -0,0 +1,119 @@
+#include <Servo.h>  //servo library
+Servo myservo;      // create servo object to control servo
+
+int Echo = A4;  
+int Trig = A5; 
+
+#define ENA 5
+#define ENB 6
+#define IN1 7
+#define IN2 8
+#define IN3 9
+#define IN4 11
+#define carSpeed 150
+#define Carspeed 110
+int rightDistance = 0, leftDistance = 0, middleDistance = 0;
+
+void Forward(){ 
+  analogWrite(ENA, Carspeed);
+  analogWrite(ENB, Carspeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  Serial.println("Forward");
+}
+
+void Back() {
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Back");
+}
+
+void Left() {
+  analogWrite(ENA, Carspeed);
+  analogWrite(ENB, Carspeed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH); 
+  Serial.println("Left");
+}
+
+void Right() {
+  analogWrite(ENA, carSpeed);
+  analogWrite(ENB, carSpeed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  Serial.println("Right");
+}
+
+void stop() {
+  digitalWrite(ENA, LOW);
+  digitalWrite(ENB, LOW);
+  Serial.println("Stop!");
+} 
+
+//Ultrasonic distance measurement Sub function
+int Distance_test() {
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance / 148;       
+  return (int)Fdistance;
+} 
+void setup() {
+  myservo.attach(3);  // attach servo on pin 3 to servo object
+  Serial.begin(9600);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  stop();
+}
+
+void loop() {
+   myservo.write(90);  //setservo position according to scaled value
+    delay(500); 
+    while(middleDistance > 6){
+    //forward();
+    middleDistance = Distance_test();} 
+      stop();
+      delay(500);  //If it's 6 inches from a wall or obstacle it will stop
+                         
+      myservo.write(10);          
+      delay(1000);      
+      rightDistance = Distance_test(); //it will look right
+      delay(500);
+      myservo.write(90);              
+      delay(1000);                                                  
+      myservo.write(180);              
+      delay(1000); 
+      leftDistance = Distance_test(); //it will look left 
+      
+      delay(500);
+      myservo.write(90);              
+      delay(1000);
+      if(rightDistance > leftDistance) {
+        Right();
+        delay(400); //It will turn right if it's closer to turning right
+      }
+      else if(rightDistance < leftDistance) {
+        Left();
+        delay(400); //It will turn left if it's closer to turning left
+      }
+    
+             }
